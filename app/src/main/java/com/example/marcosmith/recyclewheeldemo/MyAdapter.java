d43941f84a72f158ea2159f37e3e5a12cc18d18d
Marco Smith <newmarkoiskewl@gmail.com>
2018-07-23 16:08:30 -0400

    Fixed Landscape Layout

--- app/src/main/java/com/example/marcosmith/recyclewheeldemo/MyAdapter.java
+++ app/src/main/java/com/example/marcosmith/recyclewheeldemo/MyAdapter.java
@@ -1,8 +1,11 @@
 package com.example.marcosmith.recyclewheeldemo;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.method.ScrollingMovementMethod;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,6 +41,21 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
         viewHolder.textViewHead.setText(listItems.getHead());
         viewHolder.textViewDesc.setText(listItems.getDesc());
+
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        ((Activity) viewHolder.textViewDesc.getContext()).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        float yInches= metrics.heightPixels/metrics.ydpi;
+        float xInches= metrics.widthPixels/metrics.xdpi;
+        double diagonalInches = Math.sqrt(xInches*xInches + yInches*yInches);
+        if (diagonalInches>=6.5){
+            viewHolder.textViewDesc.setTextSize(30);
+        }else{
+            viewHolder.textViewDesc.setTextSize(15);
+        }
+
+        viewHolder.textViewDesc.setMovementMethod(new ScrollingMovementMethod());
 
         Picasso.with(context)
         .load(listItems.getImageUrl())
